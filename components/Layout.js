ca87f2eb7c70e69ea31201831f0792c580df7b12
ozgeboztass <123666735+ozgeboztass@users.noreply.github.com>
2024-01-16 13:54:21 +0300

    *

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,16 @@
-const Layout = () => {
+import { Sora } from "@next/font/google";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+import Nav from "../components/Nav";
+import Header from "./Header";
+import TopLeftImg from "../components/TopLeftImg";
+
+const Layout = ({ childeren }) => {
   return <div>Layout</div>;
 };
 
